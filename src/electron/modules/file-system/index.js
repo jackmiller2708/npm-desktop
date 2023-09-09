5fd67b9f0597eed0838a9c1debb21207f217f79b
Jack Miller <jackmiller0899@gmail.com>
2023-09-09 21:25:19 +0700

    Update file system helper module JSDoc

--- src/electron/modules/file-system/index.js
+++ src/electron/modules/file-system/index.js
@@ -2,6 +2,14 @@ const { writeFileSync, readFileSync } = require("fs");
 const { Either } = require("../../shared/monads/either.monad");
 const { IO } = require("../../shared/monads/io.monad");
 
+/**
+ * Reads a file synchronously from the given file path and returns an IO monad
+ * containing an Either monad representing the file's content (Right) or an error (Left).
+ *
+ * @param {string} filePath - The path to the file to read.
+ * @returns {IO} - An IO monad that, when executed with .run(), returns an Either monad representing
+ * the file content (Right) or an error (Left).
+ */
 function readFile(filePath) {
   return IO(() => {
     try {
@@ -12,6 +20,16 @@ function readFile(filePath) {
   });
 }
 
+
+/**
+ * Writes data to a file synchronously at the given file path and returns an IO monad
+ * containing an Either monad representing the written data (Right) or an error (Left).
+ *
+ * @param {string} filePath - The path to the file to write.
+ * @param {any} data - The data to write to the file.
+ * @returns {IO} - An IO monad that, when executed with .run(), returns an Either monad representing
+ * the written data (Right) or an error (Left).
+ */
 function writeFile(filePath, data) {
   return IO(() => {
     try {
