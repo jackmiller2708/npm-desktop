00ca9af03206e29ab1dc059ab97e653ba05fa79d
Jack Miller <jackmiller0899@gmail.com>
2023-09-10 15:00:53 +0700

    Fix bugs

--- src/electron/modules/ipc-listener/index.js
+++ src/electron/modules/ipc-listener/index.js
@@ -1,4 +1,6 @@
 const { getHistory, addToHistory, setLastOpened } = require("../workspace-history");
+const { ipcMain, dialog, BrowserWindow } = require("electron")
+const { validatePathThenUpdateHistory } = require("./_service");
 const { Workspace } = require("../../shared/models/workspace.model");
 const { basename } = require("path");
 
@@ -15,11 +17,13 @@ function initIPCListeners(window) {
   });
 
   ipcMain.on("open-workspace", async () => {
-    const updateFn = () => addToHistory(Workspace({ path, name: basename(path), timestamp: Date.now() }));
     const result = await dialog.showOpenDialog(window, { properties: ["openDirectory"] });
-
+    
     if (!result.canceled && result.filePaths.length > 0) {
-      validatePathThenUpdateHistory(result.filePaths[0], updateFn).fold(
+      const workspacePath = result.filePaths[0];
+      const updateFn = () => addToHistory(Workspace({ path: workspacePath, name: basename(workspacePath), timestamp: Date.now() }));
+
+      validatePathThenUpdateHistory(workspacePath, updateFn).fold(
         (error) => error,
         (data) => window.webContents.send("workspace-history-loaded", JSON.stringify(data))
       );
