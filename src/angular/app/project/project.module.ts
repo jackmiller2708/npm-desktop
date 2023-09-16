c28078d15489e0eb15c68bdbf5f3fe2ba61e698c
Jack Miller <jackmiller0899@gmail.com>
2023-09-16 21:04:53 +0700

    Minor Updates

--- src/angular/app/project/project.module.ts
+++ src/angular/app/project/project.module.ts
@@ -1,12 +1,18 @@
 import { ProjectRoutingModule } from './project-routing.module';
 import { ButtonComponent } from 'src/angular/shared/components/atoms/button/button.component';
 import { ProjectComponent } from './project.component';
+import { NgOptimizedImage } from '@angular/common';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 
 @NgModule({
   declarations: [ProjectComponent],
-  imports: [CommonModule, ProjectRoutingModule, ButtonComponent],
+  imports: [
+    CommonModule,
+    ProjectRoutingModule,
+    ButtonComponent,
+    NgOptimizedImage,
+  ],
 })
 export class ProjectModule {}
--- src/angular/shared/components/organisms/git-workspace-history/git-workspace-history.component.ts
+++ src/angular/shared/components/organisms/git-workspace-history/git-workspace-history.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-git-workspace-history',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './git-workspace-history.component.html',
-  styleUrls: ['./git-workspace-history.component.scss']
+  styleUrls: ['./git-workspace-history.component.scss'],
 })
 export class GitWorkspaceHistoryComponent {
-
+  @HostBinding('class')
+  private get _classes(): string[] {
+    return ['flex', 'items-center', 'justify-center'];
+  }
 }
