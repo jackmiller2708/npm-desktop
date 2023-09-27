d129f628f4814f8efad56c532db09cebd8912239
Jack Miller <jackmiller0899@gmail.com>
2023-09-27 14:36:42 +0700

    Apply title bar component

--- src/angular/app/app.component.ts
+++ src/angular/app/app.component.ts
@@ -1,8 +1,8 @@
-import { ChangeDetectionStrategy, Component, HostBinding, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, OnDestroy, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Subject, debounceTime, fromEvent, map, merge } from 'rxjs';
 import { InterProcessCommunicator } from '@services/IPC/inter-process-communicator.service';
 import { ToastService } from '@services/toast/toast.service';
 import { IToastItem } from '@components/molecules/display-toast/interfaces/toast-item.interface';
-import { Subject } from 'rxjs';
 import { Helper } from '@shared/helper.class';
 
 @Component({
@@ -13,16 +13,37 @@ import { Helper } from '@shared/helper.class';
 })
 export class AppComponent implements OnInit, OnDestroy {
   private readonly _ngDestroy$: Subject<void>;
+  private _isWindowActive: boolean;
+  private _isWindowMaximize: boolean;
 
   @HostBinding('class')
   private get _classes(): string[] {
-    return ['h-screen', 'w-screen', 'block', 'flex', 'flex-col', 'items-center', 'justify-center', 'relative', 'overflow-hidden'];
+    return ['h-screen', 'w-screen', 'flex', 'flex-col'];
+  }
+
+  private get _isWindowMaximized(): boolean {
+    return (
+      window.innerWidth === screen.availWidth &&
+      window.innerHeight === screen.availHeight
+    );
+  }
+
+  get isWindowActive(): boolean {
+    return this._isWindowActive;
+  }
+
+
+  get isWindowMaximize(): boolean {
+    return this._isWindowMaximize;
   }
 
   constructor(
-    private readonly _IPC: InterProcessCommunicator, 
-    private readonly _toastService: ToastService
+    private readonly _IPC: InterProcessCommunicator,
+    private readonly _toastService: ToastService,
+    private readonly _CDR: ChangeDetectorRef
   ) {
+    this._isWindowMaximize = this._isWindowMaximized;
+    this._isWindowActive = document.hasFocus();
     this._ngDestroy$ = new Subject();
   }
 
@@ -32,6 +53,16 @@ export class AppComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this._ngDestroy$.next();
+  }
+
+  private _handleWindowResize() {
+    this._isWindowMaximize = this._isWindowMaximized;
+    this._CDR.detectChanges();
+  }
+
+  private _handleWindowVisibilityChange(isFocus: boolean): void {
+    this._isWindowActive = isFocus;
+    this._CDR.detectChanges();
   }
 
   private _handleError(error: Record<string, any>) {
@@ -44,8 +75,16 @@ export class AppComponent implements OnInit, OnDestroy {
 
   private _initStores() {
     const _register = Helper.makeObservableRegistrar.call(this, this._ngDestroy$);
+
     const error$ = this._IPC.on<Record<string, any>>('system-error');
+    const windowResize$ = fromEvent(window, 'resize').pipe(debounceTime(100));
+    const windowVisibilityChange$ = merge(
+      fromEvent(window, 'focus').pipe(map(() => true)),
+      fromEvent(window, 'blur').pipe(map(() => false))
+    ); 
 
     _register(error$, this._handleError);
+    _register(windowVisibilityChange$, this._handleWindowVisibilityChange);
+    _register(windowResize$, this._handleWindowResize);
   }
 }
--- src/angular/app/app.module.ts
+++ src/angular/app/app.module.ts
@@ -1,5 +1,6 @@
 import { LoaderScreenComponent } from '../shared/components/molecules/loader-screen/loader-screen.component';
 import { DisplayToastComponent } from '../shared/components/molecules/display-toast/display-toast.component';
+import { BarTitleComponent } from '@shared/components/molecules/bar-title/bar-title.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
@@ -7,7 +8,13 @@ import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LoaderScreenComponent, DisplayToastComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LoaderScreenComponent,
+    DisplayToastComponent,
+    BarTitleComponent,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
