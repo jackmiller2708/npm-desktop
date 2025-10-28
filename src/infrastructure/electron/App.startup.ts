eb3229942d613ae20675116bb0259a97853649bb
Jack Miller <jackmiller0899@gmail.com>
2025-10-28 16:41:43 +0700

    Update App startup

--- src/infrastructure/electron/App.startup.ts
+++ src/infrastructure/electron/App.startup.ts
@@ -1,21 +1,20 @@
 import { AppStarter } from "@application/app";
-import { NpmHandler, WindowHandler } from "@application/ipc/handlers";
+import { NpmHandler } from "@application/ipc/npm";
+import { WindowHandler } from "@application/ipc/window";
 import { IpcRegistrar } from "@core/ipc";
-
 import type { IPCRegistry } from "@shared/ipc/registry";
-
 import { Effect, Layer } from "effect";
 import { MainWindow } from "./windows/main-window";
 
 export const AppStarterLive = Layer.effect(AppStarter, Effect.Do.pipe(
   Effect.andThen(() => Effect.all([IpcRegistrar, MainWindow], { concurrency: 'unbounded' })),
   Effect.map(([ipcRegistrar, mainWindow]) => AppStarter.of({
-    startup: () => Effect.Do.pipe(
-      Effect.andThen(() => ipcRegistrar.register<IPCRegistry>({
+    startup: () => Effect.zipRight(
+      ipcRegistrar.register<IPCRegistry>({
         npm: NpmHandler,
         window: WindowHandler
-      })),
-      Effect.andThen(() => mainWindow.create())
+      }),
+      mainWindow.create()
     ),
   })),
 ));
