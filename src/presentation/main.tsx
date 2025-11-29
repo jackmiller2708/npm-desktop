1a47ed6c717d2605561029e1d7698e7410eedd86
Jack Miller <jackmiller0899@gmail.com>
2025-11-30 00:11:20 +0700

    Migrate root store provider location

--- src/presentation/main.tsx
+++ src/presentation/main.tsx
@@ -4,11 +4,16 @@ import { Effect } from "effect";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import { routes } from "./app.routes";
+import { RootStoreProvider } from "./stores/root";
 
 const createAppRoot = Effect.sync(() => createRoot(document.body));
 const createAppRouter = Effect.sync(() => createBrowserRouter(routes));
 
 Effect.runPromise(Effect.Do.pipe(
   Effect.andThen(() => Effect.all([createAppRoot, createAppRouter], { concurrency: "unbounded" })), 
-  Effect.andThen(([root, router]) => Effect.sync(() => root.render(<RouterProvider router={router} />)))),
+  Effect.andThen(([root, router]) => Effect.sync(() => root.render((
+    <RootStoreProvider>
+      <RouterProvider router={router} />
+    </RootStoreProvider>
+  ))))),
 );
--- src/presentation/modules/root.tsx
+++ src/presentation/modules/root.tsx
@@ -1,18 +1,15 @@
 import { WindowFrame } from "@presentation/components/layout/window-frame";
 import { ThemeProvider } from "@presentation/components/theme-provider";
 import { Toaster } from "@presentation/components/ui/sonner";
-import { RootStoreProvider } from "@presentation/stores/root";
 import { Outlet } from "react-router";
 
 export function Root() {
 	return (
 		<ThemeProvider defaultTheme="system" storageKey="ui-theme">
-			<RootStoreProvider>
-				<WindowFrame>
-					<Outlet />
-					<Toaster />
-				</WindowFrame>
-			</RootStoreProvider>
+			<WindowFrame>
+				<Outlet />
+				<Toaster />
+			</WindowFrame>
 		</ThemeProvider>
 	);
 }
